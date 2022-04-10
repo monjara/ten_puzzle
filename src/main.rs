881105292c5e82bc023cae2bc6b51cedce0938d4
monjara <yaginumee@gmail.com>
2022-04-11 00:48:43 +0900

    add struct

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,46 @@
 use proconio::input;
 use itertools::{iproduct, Itertools};
 
-fn add(a: f32, b:f32)  -> (f32, String, bool) { (a + b, String::from("+"), false) }
-fn sub(a: f32, b:f32)  -> (f32, String, bool) { (a - b, String::from("-"), false) }
-fn rsub(a: f32, b:f32) -> (f32, String, bool) { (b - a, String::from("-"), true) }
-fn mul(a: f32, b:f32)  -> (f32, String, bool) { (a * b, String::from("*"), false) }
-fn div(a: f32, b:f32)  -> (f32, String, bool) { (a / b, String::from("/"), false) }
-fn rdiv(a: f32, b:f32) -> (f32, String, bool) { (b / a, String::from("/"), true) }
+struct Output {
+    output_id:      u32,
+    first_number:   f32,
+    second_number:  f32,
+    third_number:   f32,
+    forth_number:   f32,
+    first_operand:  char,
+    second_operand: char,
+    third_operand:  char,
+}
 
-
-fn output_1(fa: f32, fb: f32, fc: f32, fd: f32, sa: String, sb: String, sc: String) {
-    println!("(({}{}{}){}{}){}{}", fa, sa, fb, sb, fc, sc, fd);
+impl Output {
+    fn formula(&self) {
+        if self.output_id == 0 {
+            println!("(({}{}{}){}{}){}{}", self.first_number, self.first_operand, self.second_number, self.second_operand, self.third_number, self.third_operand, self.forth_number);
+        }
+        else if self.output_id == 1 {
+            println!("({}{}({}{}{})){}{}", self.first_number, self.first_operand, self.second_number, self.second_operand, self.third_number, self.third_operand, self.forth_number);
+        }
+        else if self.output_id == 2 {
+            println!("{}{}(({}{}{}){}{})", self.first_number, self.first_operand, self.second_number, self.second_operand, self.third_number, self.third_operand, self.forth_number);
+        }
+        else if self.output_id == 3 {
+            println!("{}{}({}{}({}{}{}))", self.first_number, self.first_operand, self.second_number, self.second_operand, self.third_number, self.third_operand, self.forth_number);
+        }
+        else if self.output_id == 4 {
+            println!("({}{}{}){}({}{}{})", self.first_number, self.first_operand, self.second_number, self.second_operand, self.third_number, self.third_operand, self.forth_number);
+        }
+    }
 }
-fn output_2(fa: f32, fb: f32, fc: f32, fd: f32, sa: String, sb: String, sc: String) {
-    println!("({}{}({}{}{})){}{}", fa, sa, fb, sb, fc, sc, fd);
-}
-fn output_3(fa: f32, fb: f32, fc: f32, fd: f32, sa: String, sb: String, sc: String) {
-    println!("{}{}(({}{}{}){}{})", fa, sa, fb, sb, fc, sc, fd);
-}
-fn output_4(fa: f32, fb: f32, fc: f32, fd: f32, sa: String, sb: String, sc: String) {
-    println!("{}{}({}{}({}{}{}))", fa, sa, fb, sb, fc, sc, fd);
-}
-fn output_5(fa: f32, fb: f32, fc: f32, fd: f32, sa: String, sb: String, sc: String) {
-    println!("({}{}{}){}({}{}{})", fa, sa, fb, sb, fc, sc, fd);
-}
+fn add(a: f32, b:f32)  -> (f32, char, bool) { (a + b, '+', false) }
+fn sub(a: f32, b:f32)  -> (f32, char, bool) { (a - b, '-', false) }
+fn rsub(a: f32, b:f32) -> (f32, char, bool) { (b - a, '-', true) }
+fn mul(a: f32, b:f32)  -> (f32, char, bool) { (a * b, '*', false) }
+fn div(a: f32, b:f32)  -> (f32, char, bool) { (a / b, '/', false) }
+fn rdiv(a: f32, b:f32) -> (f32, char, bool) { (b / a, '/', true) }
 
 fn main() {
     input! { v: [f32; 4] }
-    let calc_funcs: [fn(f32, f32) -> (f32, String, bool); 6] = [add, sub, rsub, mul, div, rdiv];
+    let calc_funcs: [fn(f32, f32) -> (f32, char, bool); 6] = [add, sub, rsub, mul, div, rdiv];
     // ((a o1 b) o2 c) 03 d
     // 4P2
     for perm in v.iter().permutations(2) {
@@ -38,48 +51,58 @@ fn main() {
         arr.append(&mut tmp);
 
         for (fn_1, fn_2, fn_3) in iproduct!(calc_funcs.iter(), calc_funcs.iter(), calc_funcs.iter()) {
-            let (ans_1, str_1, reverse_1) = fn_1(*arr[0], *arr[1]);
-            let (ans_2, str_2, reverse_2) = fn_2(ans_1, *arr[2]);
-            let (ans_3, str_3, reverse_3) = fn_3(ans_2, *arr[3]);
+            let (ans_1, ope_1, reverse_1) = fn_1(*arr[0], *arr[1]);
+            let (ans_2, ope_2, reverse_2) = fn_2(ans_1, *arr[2]);
+            let (ans_3, ope_3, reverse_3) = fn_3(ans_2, *arr[3]);
             if ans_3 == 10.0 {
-                let (sa, sb, sc) = if reverse_3 {
+                let (first_operand, second_operand, third_operand) = if reverse_3 {
                     if reverse_2 {
-                        (str_3, str_2, str_1)
+                        (ope_3, ope_2, ope_1)
                     } else {
-                        (str_3, str_1, str_2)
+                        (ope_3, ope_1, ope_2)
                     }
                 } else {
                     if reverse_2 {
-                        (str_2, str_1, str_3)
+                        (ope_2, ope_1, ope_3)
                     } else {
-                        (str_1, str_2, str_3)
+                        (ope_1, ope_2, ope_3)
                     }
                 };
-                
-                if !reverse_1 && !reverse_2 && !reverse_3 {
-                    output_1(*arr[0], *arr[1], *arr[2], *arr[3], sa, sb, sc);
-                }
-                else if reverse_1 && !reverse_2 && !reverse_3 {
-                    output_1(*arr[1], *arr[0], *arr[2], *arr[3], sa, sb, sc);
-                }
-                else if reverse_1 && reverse_2 && !reverse_3 {
-                    output_2(*arr[2], *arr[1], *arr[0], *arr[3], sa, sb, sc);
-                }
-                else if !reverse_1 && reverse_2 && !reverse_3 {
-                    output_2(*arr[2], *arr[0], *arr[1], *arr[3], sa, sb, sc);
-                }
-                else if reverse_1 && !reverse_2 && reverse_3 {
-                    output_3(*arr[3], *arr[1], *arr[0], *arr[2], sa, sb, sc);
-                }
-                else if !reverse_1 && !reverse_2 && reverse_3 {
-                    output_3(*arr[3], *arr[0],*arr[1],  *arr[2], sa, sb, sc);
-                }
-                else if !reverse_1 && reverse_2 && reverse_3 {
-                    output_4(*arr[3], *arr[2], *arr[0], *arr[1], sa, sb, sc);
-                }
-                else if reverse_1 && reverse_2 && reverse_3 {
-                    output_4(*arr[3], *arr[2], *arr[1], *arr[0], sa, sb, sc);
-                }
+
+                let (output_id, 
+                    first_number, 
+                    second_number, 
+                    third_number, 
+                    forth_number
+                ) = if !reverse_1 && !reverse_2 && !reverse_3 {
+                    (0, *arr[0], *arr[1], *arr[2], *arr[3])
+                } else if reverse_1 && !reverse_2 && !reverse_3 {
+                    (0, *arr[1], *arr[0], *arr[2], *arr[3])
+                } else if !reverse_1 && reverse_2 && !reverse_3 {
+                    (1, *arr[2], *arr[0], *arr[1], *arr[3])
+                } else if reverse_1 && reverse_2 && !reverse_3 {
+                    (1, *arr[2], *arr[1], *arr[0], *arr[3])
+                } else if reverse_1 && !reverse_2 && reverse_3 {
+                    (2, *arr[3], *arr[1], *arr[0], *arr[2])
+                } else if !reverse_1 && !reverse_2 && reverse_3 {
+                    (2, *arr[3], *arr[0],*arr[1],  *arr[2])
+                } else if !reverse_1 && reverse_2 && reverse_3 {
+                    (3, *arr[3], *arr[2], *arr[0], *arr[1])
+                } else {
+                    (3, *arr[3], *arr[2], *arr[1], *arr[0])
+                };
+
+                let output = Output { 
+                    output_id,
+                    first_number, 
+                    second_number, 
+                    third_number, 
+                    forth_number, 
+                    first_operand, 
+                    second_operand, 
+                    third_operand,
+                };
+                output.formula();
             }
         }
     }
@@ -96,30 +119,56 @@ fn main() {
 
     for arr in arrs.iter() {
         for (fn_1, fn_2, fn_3) in iproduct!(calc_funcs.iter(), calc_funcs.iter(), calc_funcs.iter()) {
-            let (ans_1, str_1, reverse_1) = fn_1(arr[0], arr[1]);
-            let (ans_2, str_2, reverse_2) = fn_2(arr[2], arr[3]);
-            let (ans_3, str_3, reverse_3) = fn_3(ans_1, ans_2);
+            let (ans_1, ope_1, reverse_1) = fn_1(arr[0], arr[1]);
+            let (ans_2, ope_2, reverse_2) = fn_2(arr[2], arr[3]);
+            let (ans_3, ope_3, reverse_3) = fn_3(ans_1, ans_2);
             if ans_3 == 10.0 {
+                let output_id = 4;
+
                 let mut ans_arr = arr.to_vec();
-
-                if reverse_1 {
+                let (
+                    first_number, 
+                    second_number, 
+                    third_number, 
+                    forth_number
+                ) = {
+                    if reverse_1 {
                     ans_arr.swap(0, 1);
-                };
-                if reverse_2 {
-                    ans_arr.swap(2, 3);
-                };
-                if reverse_3 { 
-                    ans_arr.swap(0, 2);
-                    ans_arr.swap(1, 3);
+                    };
+                    if reverse_2 {
+                        ans_arr.swap(2, 3);
+                    };
+                    if reverse_3 { 
+                        ans_arr.swap(0, 2);
+                        ans_arr.swap(1, 3);
+                    };
+                    (ans_arr[0], ans_arr[1], ans_arr[2], ans_arr[3])
                 };
 
-                let (sa, sb, sc) = if reverse_3 {
-                    (str_2, str_3, str_1)
+                let (
+                    first_operand, 
+                    second_operand, 
+                    third_operand
+                ) = if reverse_3 {
+                    (ope_2, ope_3, ope_1)
                 } else {
-                    (str_1, str_3, str_2)
+                    (ope_1, ope_3, ope_2)
                 };
-                output_5(ans_arr[0], ans_arr[1], ans_arr[2], ans_arr[3], sa, sb, sc);
+
+                let output = Output {
+                    output_id,
+                    first_number,
+                    second_number,
+                    third_number,
+                    forth_number,
+                    first_operand,
+                    second_operand,
+                    third_operand,
+                };
+
+                output.formula();
             }
         }
     }
 }
+
